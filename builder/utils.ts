8e6619f7386d367dd651ac0df5ef96928ae8f22c
DogmaDragon <103123951+DogmaDragon@users.noreply.github.com>
2025-11-22 15:51:23 +0200

    Retire builder, thanks @feedberbox826 for all the work on it

--- builder/utils.ts
+++
@@ -1,5 +0,0 @@
-// utility functions for the builder
-export const sanitizeMD = (md: string) => md.replace("<", "&lt;").replace(">", "&gt;")
-export const infoLog = (message: string) => console.log(`[INFO] ${message}`)
-export const warnLog = (message: string) => console.warn(`[WARN] ${message}`)
-export const debugLog = (message: string) => console.debug(`[DEBUG] ${message}`)
